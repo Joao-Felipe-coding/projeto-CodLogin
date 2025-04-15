a4a2bc8b0288dc692a1ee688b6613fbd71d0207f
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-04-15 19:39:41 -0300

    função acessar, Implementa acesso para a tela de login

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
 //Implemeta acesso para a tela de login
+function acessar(){
+  let loginEmail = document.getElementById("loginEmail").value;
+  let loginSenha = document.getElementById("loginSenha").value;
+
+  
+}
