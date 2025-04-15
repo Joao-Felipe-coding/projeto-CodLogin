71e84bb77fe59b97c06a7ffef8bc1866f7efb22c
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-04-15 19:56:21 -0300

    se preencher todos os campos, ele levará para a tela de cadastro

--- main.js
+++ main.js
@@ -1,12 +1,12 @@
 //Implemeta acesso para a tela de login
-function acessar(){
+function acessar() {
   let loginEmail = document.getElementById("loginEmail").value;
   let loginSenha = document.getElementById("loginSenha").value;
 
   //implementa estrutura que testa se todos os campos foram preenchidos
-  if(!loginEmail || !loginSenha){
+  if (!loginEmail || !loginSenha) {
     alert("Preencha todos os campos!");
   } else {
-    alert("Login realizado com sucesso!");
+    window.location.href = "cadastro.html";
   }
 }
