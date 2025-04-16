d22b3e242f73e5adf79cbc9715a008b5e7cdea23
Joao-Felipe-coding <jofelipe2018@gmail.com>
2025-04-15 21:32:30 -0300

    termino de função criaLista() e inicio de função excluir() em main.js

--- main.js
+++ main.js
@@ -1,12 +1,13 @@
-//Implemeta acesso para a tela de login
+// Configura acesso para a tela de login
 function acessar() {
   let loginEmail = document.getElementById("loginEmail").value;
   let loginSenha = document.getElementById("loginSenha").value;
 
-  //implementa estrutura que testa se todos os campos foram preenchidos
+  // Implementa estrutura que testa se todos os campos foram preenchidos
   if (!loginEmail || !loginSenha) {
     alert("Preencha todos os campos!");
-  } else {
+  }
+  else {
     window.location.href = "cadastro.html";
   }
 }
@@ -14,11 +15,37 @@ function acessar() {
 //cria variável do tipo array
 var dadosLista = [];
 
-function salvarUser(){
+function salvarUser() {
   let nomeUser = document.getElementById("nomeUser").value;
 
-  if(nomeUser){
+  if (nomeUser) {
     dadosLista.push(nomeUser);
-    
+    console.log(dadosLista);
+    criaLista();
+    document.getElementById('nomeUser').valeu = "";
+  } else {
+    alert("Favor informar um nome para o cadastro");
   }
+}
+
+//Função para preencher a lista de cadastro
+function criaLista() {
+  let tabela = "<tr><th>Nome</th><th>Ações</th></tr>";
+  for (let i = 0; i <= (dadosLista.length - 1); i++) {
+    tabela += `
+    <tr>
+      <td>${dadosLista[i]}</td>
+      <td>
+        <button class='btn btn btn-warning'>Editar</button>
+        <button class='btn btn btn-danger'>Excluir</button>
+      </td>
+    </tr>`;
+    document.getElementById('tabela').innerHTML = tabela;
+  }
+}
+
+//Função para excluir nome da lista
+function excluir(i) {
+  dadosLista.splice((i-1), 1);
+  criaLista();
 }
